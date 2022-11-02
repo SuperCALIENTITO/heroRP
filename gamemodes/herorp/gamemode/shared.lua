73392e237ef6e25c1d841a38a4bb80469112542a
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-02 13:24:49 -0300

    Update shared.lua

--- gamemodes/herorp/gamemode/shared.lua
+++ gamemodes/herorp/gamemode/shared.lua
@@ -3,3 +3,13 @@ GM.Author = "Vicentefelipechile & Lugent"
 GM.Website = "www.mapping-latam.cl"
 
 DeriveGamemode( "sandbox" )
+
+function GM:Initialize()
+
+    self.BaseClass.Initialize(self)
+
+    if not sql.TableExists("herorp_player") then
+        sql.Query("CREATE TABLE IF NOT EXISTS herorp_player")
+    end
+    
+end
