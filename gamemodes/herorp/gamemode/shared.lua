dcf5768c54201f827f980f170c6faac906ce0eef
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-03 22:58:09 -0300

    commit

--- gamemodes/herorp/gamemode/shared.lua
+++ gamemodes/herorp/gamemode/shared.lua
@@ -9,8 +9,6 @@ function GM:Initialize()
 
     self.BaseClass.Initialize(self)
 
-    if not sql.TableExists("herorp_player") then
-        sql.Query("CREATE TABLE IF NOT EXISTS herorp_player")
-    end
+    RunConsoleCommand("sbox_noclip", "0")
     
 end
